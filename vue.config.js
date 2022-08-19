554ec59fbed6547526038ea5e889375bb203791b
pantelis <kamanas@zeusplay.com>
2022-08-19 17:15:44 +0300

    File vue.config.js added...

---
+++ vue.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    pluginOptions: {
+        electronBuilder: {
+            builderOptions: {
+                appId: 'test.com',
+                win: {
+                    icon: 'assets/spineboy.ico'
+                }
+            }
+        }
+    }
+}
